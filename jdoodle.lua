2634530557bead88cd97c01bc3437c78bf09c841
Johan Antonio <147890607+johansitoweb@users.noreply.github.com>
2025-01-14 00:40:18 -0400

    lua

---
+++ jdoodle.lua
@@ -0,0 +1,15 @@
+print("Ingrese el primer número:")
+num1 = io.read("*n")
+print("Ingrese el segundo número:")
+num2 = io.read("*n")
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("La suma es:", suma)
+print("La resta es:", resta)
+print("La multiplicación es:", multiplicacion)
+print("La división es:", division)
+
